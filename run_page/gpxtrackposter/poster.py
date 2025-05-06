2ce388a8d07a72a75aa1a933f9cc384cde7f8278
Hanlin He <hanling.he@gmail.com>
2025-05-05 20:39:30 -0700

    Personal updates.

--- run_page/gpxtrackposter/poster.py
+++ run_page/gpxtrackposter/poster.py
@@ -147,14 +147,14 @@ class Poster:
 
     def __draw_header(self, d):
         text_color = self.colors["text"]
-        title_style = "font-size:12px; font-family:Arial; font-weight:bold;"
+        title_style = "font-size:12px; font-family:'Lato'; font-weight:bold;"
         d.add(d.text(self.title, insert=(10, 20), fill=text_color, style=title_style))
 
     def __draw_footer(self, d):
         text_color = self.colors["text"]
-        header_style = "font-size:4px; font-family:Arial"
-        value_style = "font-size:9px; font-family:Arial"
-        small_value_style = "font-size:3px; font-family:Arial"
+        header_style = "font-size:4px; font-family:'Lato'"
+        value_style = "font-size:9px; font-family:'Lato'"
+        small_value_style = "font-size:3px; font-family:'Lato'"
 
         special_distance1 = self.special_distance["special_distance"]
         special_distance2 = self.special_distance["special_distance2"]
